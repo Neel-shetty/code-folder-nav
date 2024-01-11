294a6a4e19fa2aa3c96fe37d8725c1298ef28c9f
Neel <neelnarayanshetty@gmail.com>
2024-01-11 23:45:00 +0530

    updates

--- main.c
+++ main.c
@@ -14,25 +14,37 @@ num_map folders[1000];
 int main() {
   DIR *d;
   char *wd = "/home/neel/code";
-  d = opendir(wd);
-  struct dirent *entries;
-  int i = 1;
-  while ((entries = readdir(d)) != NULL) {
-    if (strcmp(entries->d_name, ".") == 0 ||
-        strcmp(entries->d_name, "..") == 0) {
+  char *prev_dir = wd;
+  int option = -1;
+  while (option != 0) {
+    d = opendir(wd);
+    if (!d) {
+      printf("unable to read dir\n");
+      wd = prev_dir;
+      option = -1;
       continue;
     }
-    printf("%d: %s\n", i, entries->d_name);
-    strcpy(folders[i].name, entries->d_name);
-    i++;
+    struct dirent *entries;
+    int i = 1;
+    while ((entries = readdir(d)) != NULL) {
+      if (strcmp(entries->d_name, ".") == 0 ||
+          strcmp(entries->d_name, "..") == 0) {
+        continue;
+      }
+      printf("%d - ", entries->d_type);
+      printf("%d: %s\n", i, entries->d_name);
+      strcpy(folders[i].name, entries->d_name);
+      i++;
+    }
+
+    printf("select the number of the folder you want to go to\n");
+    scanf("%d", &option);
+    char *cd;
+    asprintf(&cd, "%s/%s", wd, folders[option].name);
+    printf("%s\n", cd);
+    prev_dir = wd;
+    wd = cd;
+    closedir(d);
   }
-
-  int option;
-  printf("select the number of the folder you want to go to\n");
-  scanf("%d", &option);
-  char *cd;
-  asprintf(&cd, "%s/%s", wd, folders[option].name);
-  printf("%s\n", cd);
-
   return 0;
 }

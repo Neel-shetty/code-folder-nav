5dd9b59b1966730d65f28fcf5d0af1dd7aaae666
Neel <neelnarayanshetty@gmail.com>
2024-01-12 19:44:18 +0530

    comments

--- main.c
+++ main.c
@@ -8,6 +8,9 @@
 #include <unistd.h>
 #include <wchar.h>
 
+#define TYPE_FOLDER 4
+#define TYPE_FILE 8
+
 typedef struct num_map {
   char name[256];
   int type;
@@ -17,6 +20,7 @@ num_map folders[1000];
 
 int main(int argc, char *argv[]) {
 
+  // init variables
   DIR *d;
   char *wd = "/home/neel/code";
   if (argc == 1) {
@@ -25,47 +29,66 @@ int main(int argc, char *argv[]) {
     printf("only takes in one argument");
   }
   char *prev_dir = wd;
-  int option = -1;
-  setlocale(LC_CTYPE, "");
-  wchar_t folder_icon = 0x2606;
-  while (option != 0) {
+
+  // option management
+  char *option = malloc(256);
+  strcpy(option, "-1");
+
+  // file menu loop
+  while (atoi(option) != 0) {
     d = opendir(wd);
+
+    // if the selected option is not a directory, prompt again.
     if (!d) {
       printf("unable to read dir\n");
       wd = prev_dir;
-      option = -1;
+      // option = -1;
+      strcpy(option, "-1");
       continue;
     }
+
+    // print menu
     struct dirent *entries;
     int i = 1;
     while ((entries = readdir(d)) != NULL) {
+      // early exit for the current dir and prev dir options
       if (strcmp(entries->d_name, ".") == 0 ||
           strcmp(entries->d_name, "..") == 0) {
         continue;
       }
-      if (entries->d_type == 4) {
+      if (entries->d_type == TYPE_FILE) {
         wprintf(L"dir   %d: %s\n", i, entries->d_name);
         // printf("%d: %s\n", i, entries->d_name);
       } else {
         wprintf(L"file  %d: %s\n", i, entries->d_name);
       }
+      // store folder info in folders array based on index
       strcpy(folders[i].name, entries->d_name);
       folders[i].type = entries->d_type;
       i++;
     }
 
+    // take input from user
     printf("select the number of the folder you want to go to\n");
-    scanf("%d", &option);
+    scanf("%s", option);
+
+    // concat string "path + folder name"
     char *cd;
-    asprintf(&cd, "%s/%s", wd, folders[option].name);
+    asprintf(&cd, "%s/%s", wd, folders[atoi(option)].name);
     printf("%s\n", cd);
+
+    // prev dir for fallback incase the option selected is a file
     prev_dir = wd;
     wd = cd;
     closedir(d);
   }
-  if (option == 0) {
+
+  // when quit
+  if (atoi(option) == 0) {
     char *command;
     char *cd_into_dir;
+
+    // concact strings (command + path)
     // asprintf(&command, "kitty --detach nvim %s", wd);
     asprintf(&cd_into_dir, "cd %s", wd);
     asprintf(&command, "nvim %s", wd);
